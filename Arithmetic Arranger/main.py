c31f83c74ba69a3e7aafa221935447071e8ab256
NKGachoki <njambagachoki@gmail.com>
2024-05-21 10:20:37 +0300

    adding README

--- Arithmetic Arranger/main.py
+++ Arithmetic Arranger/main.py
@@ -1,4 +1,5 @@
-from Arithmetic_Arranger import arithmetic_arranger
+"""Module providing a function that arranges and solves the list of arithmetic problems."""
+from arithmetic_arranger import arithmetic_arranger
 
 print("Output of arithmetic_arranger function where 2nd arg == 'False'")
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
